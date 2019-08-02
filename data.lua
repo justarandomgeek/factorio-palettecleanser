ca14af117d7f6c49eeb03a10256dc65f94c469f6
okradonkey <okradonkey@disroot.org>
2019-08-02 04:30:05 -0500

    Disabled most settings, left essentials.
    Only modify overlays for turret, grenade, logistics, and construction.

--- data.lua
+++ data.lua
@@ -1,40 +1,46 @@
 
-local function read_color_settings(name)
-  return settings.startup["palette-cleanser-"..name].value
-end
+-- local function read_color_settings(name)
+--   return settings.startup["palette-cleanser-"..name].value
+-- end
 
-data.raw["utility-constants"].default.turret_range_visualization_color = read_color_settings("turret-range")
-data.raw["utility-constants"].default.chart.turret_range_color = read_color_settings("turret-range-chart")
-data.raw["utility-constants"].default.capsule_range_visualization_color= read_color_settings("capsule-range")
+--  data.raw["utility-constants"].default.turret_range_visualization_color = read_color_settings("turret-range")
+data.raw["utility-constants"].default.turret_range_visualization_color = {r = 0.1, g = 0.1, b = 0.3, a = .15}
+
+-- data.raw["utility-constants"].default.chart.turret_range_color = read_color_settings("turret-range-chart")
+
+-- data.raw["utility-constants"].default.capsule_range_visualization_color= read_color_settings("capsule-range")
+data.raw["utility-constants"].default.capsule_range_visualization_color= {r = 0.1, g = 0.1, b = 0.3, a = 0.15}
 
 data.raw["utility-sprites"].default.logistic_radius_visualization.filename = "__palettecleanser__/graphics/visualization-roboport-radius.png"
-data.raw["utility-sprites"].default.logistic_radius_visualization.tint = read_color_settings("roboport-logistic")
+-- data.raw["utility-sprites"].default.logistic_radius_visualization.tint = read_color_settings("roboport-logistic")
+data.raw["utility-sprites"].default.logistic_radius_visualization.tint = {r = 0.5, g = 0, b = 0.5}
 
 data.raw["utility-sprites"].default.construction_radius_visualization.filename = "__palettecleanser__/graphics/visualization-roboport-radius.png"
-data.raw["utility-sprites"].default.construction_radius_visualization.tint = read_color_settings("roboport-construction")
+-- data.raw["utility-sprites"].default.construction_radius_visualization.tint = read_color_settings("roboport-construction")
+data.raw["utility-sprites"].default.construction_radius_visualization.tint = {r = 0, g = 0.5, b = 0.5}
 
-data.raw["utility-sprites"].default.pollution_visualization.filename = "__palettecleanser__/graphics/pollution-visualization.png"
-data.raw["utility-sprites"].default.pollution_visualization.tint = read_color_settings("pollution")
+-- data.raw["utility-sprites"].default.pollution_visualization.filename = "__palettecleanser__/graphics/pollution-visualization.png"
+-- data.raw["utility-sprites"].default.pollution_visualization.tint = read_color_settings("pollution")
 
-data.raw['mining-drill']['electric-mining-drill'].radius_visualisation_picture.filename = "__palettecleanser__/graphics/electric-mining-drill-radius-visualization.png"
-data.raw['mining-drill']['electric-mining-drill'].radius_visualisation_picture.tint = read_color_settings("mining-drill-radius")
+-- data.raw['mining-drill']['electric-mining-drill'].radius_visualisation_picture.filename = "__palettecleanser__/graphics/electric-mining-drill-radius-visualization.png"
+-- data.raw['mining-drill']['electric-mining-drill'].radius_visualisation_picture.tint = read_color_settings("mining-drill-radius")
 
-data.raw['electric-pole']['small-electric-pole'].radius_visualisation_picture.filename = "__palettecleanser__/graphics/electric-pole-radius-visualization.png"
-data.raw['electric-pole']['small-electric-pole'].radius_visualisation_picture.tint = read_color_settings("electric-pole-radius")
+-- data.raw['electric-pole']['small-electric-pole'].radius_visualisation_picture.filename = "__palettecleanser__/graphics/electric-pole-radius-visualization.png"
+-- data.raw['electric-pole']['small-electric-pole'].radius_visualisation_picture.tint = read_color_settings("electric-pole-radius")
 
-data.raw['electric-pole']['medium-electric-pole'].radius_visualisation_picture.filename = "__palettecleanser__/graphics/electric-pole-radius-visualization.png"
-data.raw['electric-pole']['medium-electric-pole'].radius_visualisation_picture.tint = read_color_settings("electric-pole-radius")
+-- data.raw['electric-pole']['medium-electric-pole'].radius_visualisation_picture.filename = "__palettecleanser__/graphics/electric-pole-radius-visualization.png"
+-- data.raw['electric-pole']['medium-electric-pole'].radius_visualisation_picture.tint = read_color_settings("electric-pole-radius")
 
-data.raw['electric-pole']['big-electric-pole'].radius_visualisation_picture.filename = "__palettecleanser__/graphics/electric-pole-radius-visualization.png"
-data.raw['electric-pole']['big-electric-pole'].radius_visualisation_picture.tint = read_color_settings("electric-pole-radius")
+-- data.raw['electric-pole']['big-electric-pole'].radius_visualisation_picture.filename = "__palettecleanser__/graphics/electric-pole-radius-visualization.png"
+-- data.raw['electric-pole']['big-electric-pole'].radius_visualisation_picture.tint = read_color_settings("electric-pole-radius")
 
-data.raw['electric-pole']['substation'].radius_visualisation_picture.filename = "__palettecleanser__/graphics/electric-pole-radius-visualization.png"
-data.raw['electric-pole']['substation'].radius_visualisation_picture.tint = read_color_settings("electric-pole-radius")
+-- data.raw['electric-pole']['substation'].radius_visualisation_picture.filename = "__palettecleanser__/graphics/electric-pole-radius-visualization.png"
+-- data.raw['electric-pole']['substation'].radius_visualisation_picture.tint = read_color_settings("electric-pole-radius")
 
-if settings.startup["palette-cleanser-signal-wire-mode"].value == "redblue" then
-  -- change green to blue
-  data.raw["utility-sprites"].default.green_wire.filename = "__palettecleanser__/graphics/blue-wire.png"
-elseif settings.startup["palette-cleanser-signal-wire-mode"].value == "bluegreen" then 
-  -- change red to blue
-  data.raw["utility-sprites"].default.red_wire.filename = "__palettecleanser__/graphics/blue-wire.png"
-end
+-- if settings.startup["palette-cleanser-signal-wire-mode"].value == "redblue" then
+--   -- change green to blue
+--   data.raw["utility-sprites"].default.green_wire.filename = "__palettecleanser__/graphics/blue-wire.png"
+-- elseif settings.startup["palette-cleanser-signal-wire-mode"].value == "bluegreen" then 
+--   -- change red to blue
+--   data.raw["utility-sprites"].default.red_wire.filename = "__palettecleanser__/graphics/blue-wire.png"
+-- end
--- settings.lua
+++ settings.lua
@@ -1,34 +1,34 @@
-local function create_color_settings(name,defaultcolor)
-  data:extend{
-    {
-      type = "string-setting",
-      name = "palette-cleanser-"..name,
-      setting_type = "startup",
-      default_value = defaultcolor,
-      order="palette-cleanser-"..name
-    },
-  }
-end
+-- local function create_color_settings(name,defaultcolor)
+--   data:extend{
+--     {
+--       type = "string-setting",
+--       name = "palette-cleanser-"..name,
+--       setting_type = "startup",
+--       default_value = defaultcolor,
+--       order="palette-cleanser-"..name
+--     },
+--   }
+-- end
 
 
-create_color_settings("pollution","ff000099")
-create_color_settings("roboport-construction","83d937a6")
-create_color_settings("roboport-logistic","ff8800a6")
+-- create_color_settings("pollution","ff000099")
+-- create_color_settings("roboport-construction","83d937a6")
+-- create_color_settings("roboport-logistic","ff8800a6")
 
-create_color_settings("turret-range","0d1a0d27")
-create_color_settings("turret-range-chart","cd3f3fff")
-create_color_settings("capsule-range","0d1a0d27")
+-- create_color_settings("turret-range","0d1a0d27")
+-- create_color_settings("turret-range-chart","cd3f3fff")
+-- create_color_settings("capsule-range","0d1a0d27")
 
-create_color_settings("mining-drill-radius","4ead9fff")
-create_color_settings("electric-pole-radius","4ead9fff")
+-- create_color_settings("mining-drill-radius","4ead9fff")
+-- create_color_settings("electric-pole-radius","4ead9fff")
 
-data:extend{
-  {
-    type = "string-setting",
-    name = "palette-cleanser-signal-wire-mode",
-    setting_type = "startup",
-    default_value = "redgreen",
-    order="palette-cleanser-signal-wire-mode",
-    allowed_values = {"redgreen", "redblue", "bluegreen"}
-  },
-}
+-- data:extend{
+--   {
+--     type = "string-setting",
+--     name = "palette-cleanser-signal-wire-mode",
+--     setting_type = "startup",
+--     default_value = "redgreen",
+--     order="palette-cleanser-signal-wire-mode",
+--     allowed_values = {"redgreen", "redblue", "bluegreen"}
+--   },
+-- }
