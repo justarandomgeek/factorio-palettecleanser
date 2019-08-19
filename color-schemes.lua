9aa199a98f96544e33cac501fd79bbd85de3ffdb
okradonkey <okradonkey@disroot.org>
2019-08-19 09:24:56 -0500

    Update color-schemes.lua
    Fixed: forgot to add tree color to Custom color scheme

--- color-schemes.lua
+++ color-schemes.lua
@@ -108,6 +108,7 @@ return {
         sand_2_chart_color =     {r = 0.41, g = 0.36, b = 0.28},  -- soya bean
         sand_3_chart_color =     {r = 0.43, g = 0.39, b = 0.31},  -- coffee
         cliff_chart_color =      {r = 0.30, g = 0.30, b = 0.40, a = 0.80},  -- cornflower blue
+        tree_chart_color =       {r = 0.35, g = 0.85, b = 0.35, a = 0.25},  -- pastel green
 
         petroleum_gas_base_color = { r = 0.90, g = 0.35, b = 0.60 }, -- dark pink
         petroleum_gas_flow_color = { r = 0.30, g = 0.10, b = 0.30 }, -- blackberry violet
